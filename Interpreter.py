2c48373122ed7b01904411466139842687345137
YogyaSrivastava134 <srivastavayogya10304@gmail.com>
2025-05-24 01:34:04 +0530

    Interpreter.py
    Task 4: Experiment with different arithmetic and logical operators in the interpreter.

---
+++ Interpreter.py
@@ -0,0 +1,12 @@
+# Arithmetic operators
+print(5 + 3)   # Addition
+print(10 - 4)  # Subtraction
+print(6 * 7)   # Multiplication
+print(8 / 2)   # Division
+print(10 % 3)  # Modulus (remainder)
+print(2 ** 3)  # Exponentiation
+
+# Logical operators
+print(5 > 3 and 2 < 4)  # True and True -> True
+print(5 > 3 or 2 > 4)   # True or False -> True
+print(not (5 > 3))      # not True -> False
