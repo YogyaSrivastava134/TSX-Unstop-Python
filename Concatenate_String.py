91f56137ce1ff6bd82e07102c95351e7bcf66cce
YogyaSrivastava134 <srivastavayogya10304@gmail.com>
2025-05-24 01:35:44 +0530

    Concatenate_String.py
    Task 5: Write a script that concatenates two strings and prints the result.

--- Concatenate_String.py
+++ Concatenate_String.py
@@ -1,1 +1,8 @@
+# This script concatenates two strings entered by the user
 
+str1 = input("Enter the first string: ")
+str2 = input("Enter the second string: ")
+
+result = str1 + str2  # Concatenation
+
+print("Concatenated result:", result)
