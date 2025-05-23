1c7c276e410b8928e8004483529f4ea655c162be
YogyaSrivastava134 <srivastavayogya10304@gmail.com>
2025-05-24 01:19:48 +0530

    Create hello.py
    Topic 1: Introduction to Python and Setting Up Your Environment

---
+++ hello.py
@@ -0,0 +1,16 @@
+# This is a simple Python script with comments explaining each part
+
+# This line prints a welcome message
+print("Hello, my name is Yogya.")
+
+# This line defines a variable
+age = 21
+
+# This line prints your age using the variable
+print("I am", age, "years old.")
+
+# This line shows how to add two numbers
+sum = 5 + 3
+
+# This prints the result of the addition
+print("The sum of 5 and 3 is", sum)
